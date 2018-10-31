f2a8d60cfaeb58680d1c454b693a86e3b63e5aec
KotlinNLP Authors <github@kotlinnlp.com>
2018-10-31 08:00:17 +0100

    Add the 'ContextCheck' interface

---
+++ src/main/kotlin/com/kotlinnlp/linguisticconditions/ContextCheck.kt
@@ -0,0 +1,33 @@
+/* Copyright 2018-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticconditions
+
+import com.kotlinnlp.linguisticdescription.morphology.SingleMorphology
+import com.kotlinnlp.linguisticdescription.sentence.token.MorphoSynToken
+
+/**
+ * Implemented by conditions that are verified looking at the context morphology.
+ */
+interface ContextCheck {
+
+  /**
+   * Whether this condition needs to look at the context morphology.
+   */
+  val checkContext: Boolean
+
+  /**
+   * @param token a single morpho-syntactic token
+   *
+   * @return the morphology of the token on which to check the agreement
+   */
+  fun getMorphology(token: MorphoSynToken.Single): SingleMorphology =
+    if (this.checkContext)
+      token.contextMorphologies.single().value
+    else
+      token.morphologies.single().value
+}
